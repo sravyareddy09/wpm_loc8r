01bfaad57fe81d5c819739e7476ca31e8345536a
sravyareddy09 <126338621+sravyareddy09@users.noreply.github.com>
2023-09-16 18:42:37 +0530

    Update locations.js

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,11 @@
 const homelist = (req, res) => {  
-    res.render('index', { title: 'Monish web page' });   
+    res.render('index', { title: 'Sravya web page' });   
 };
 const locationInfo = (req, res) => {  
-    res.render('index', { title: 'Monish web page' });   
+    res.render('index', { title: 'Sravya web page' });   
 };
 const addReview = (req, res) => {  
-    res.render('index', { title: 'Monish web page' });   
+    res.render('index', { title: 'Sravya web page' });   
 };
 module.exports = {
     homelist,
