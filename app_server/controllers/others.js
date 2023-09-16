bbbe894d9ac2141382068383f710c9fc2b300519
sravyareddy09 <126338621+sravyareddy09@users.noreply.github.com>
2023-09-16 18:43:26 +0530

    Update others.js

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,5 @@
 const about = (req, res) => {  
-    res.render('index', { title: 'Monish web page' });   
+    res.render('index', { title: 'Sravya web page' });   
 };
 module.exports = {
     about
